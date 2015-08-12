e4303102deca400fd1f2b37b9cac641f60974d5a
root <root@localhost.localdomain>
2015-08-12 19:33:16 +0800

    a new version of packing and unpacking binary data

--- data_test.go
+++ data_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func _Pack(v interface{}) {
-	data, err := Pack(v)
+func _Pack(v ...interface{}) {
+	data, err := Pack(v...)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -17,4 +17,5 @@ func Test_pack(t *testing.T) {
 	_Pack("hello world!")
 	buf := []byte{104, 101, 108, 111}
 	_Pack(buf)
+	_Pack(3, "hello world!", buf)
 }
